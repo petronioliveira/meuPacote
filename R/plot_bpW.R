eef069b3ac8f7fbb72356aef2760b963c1533a44
Petronio Fagundes de Oliveira Filho <petronioliveira@gmail.com>
2022-09-04 14:59:12 -0300

    Commit wilcox com bp

--- R/plot_bpW.R
+++ R/plot_bpW.R
@@ -1,6 +1,7 @@
-#'Função de teste t com boxplots
+#'Função que desenha boxplots comparando duas médias com teste de Wilcox
 #'
-#'Esta calcula um teste t e desenha os boxplots com o resultado do teste
+#'Esta calcula um teste de Wilcox para duas médias independentes e desenha os
+#' boxplots com o resultado do teste
 #'
 #'@param df é um dataframe com os dados
 #'@param var.x é uma variável categórica dicotômica
@@ -8,15 +9,15 @@
 #'
 #'@example
 #'
-#'plot_bp (df, var.x, var.y)
+#'plot_bpW (df, var.x, var.y)
 #'
 #'@export
-plot_bp <- function(df, var.x, var.y){
+plot_bpW <- function(df, var.x, var.y){
   library(ggplot2)
   ggplot(df, aes(x = {{var.x}}, y = {{var.y}}, fill = {{var.x}})) +
     geom_errorbar(stat = "boxplot", width = 0.1) +
     geom_boxplot() +
     theme_classic() +
     theme(legend.position = "none") +
-    stat_compare_means(method = "t.test", label.x = 0.5)
+    stat_compare_means(method = "wilcox.test", label.x = 0.5)
 }
