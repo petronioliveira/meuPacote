df41defa1c3e6dd1c9b2ed9c24b0415c6daace96
Petronio Fagundes de Oliveira Filho <petronioliveira@gmail.com>
2022-09-05 09:52:29 -0300

    Commit correção sumario Grupos

--- R/sumarioGrupos.R
+++ R/sumarioGrupos.R
@@ -7,21 +7,16 @@
 #' @param y é a variável numérica que será resumida
 #'
 #' @export
-sumarioGrupos <- function(data = NULL, var.grupo = NULL, y = NULL){
-
-  if(is.null(data)) stop("Informar dados")
-  if(is.null(var.grupo)) stop("Informar a variável grupo")
-  if(is.null(y)) stop("Informar os dados a variável numérica y")
-
-  sumario <- data %>%
-    group_by({{var.grupo}}) %>%
-    summarise(n = n (),
-              media = mean({{y}}, na.rm = TRUE),
-              dp = sd ({{y}}, na.rm = TRUE),
-              mediana = median ({{y}}, na.rm = TRUE),
-              IIQ = IQR ({{y}}, na.rm = TRUE),
-              ic = std.error(y)*qt(1 - (0.05/2), n - 1),
-              linf = media - ic,
-              lsup = media + ic)
-  print (sumario)
+sumarioGrupos <- function(dados = NULL, var.grupo = NULL, var.teste = NULL){
+      dados %>%
+      group_by({{var.grupo}}) %>%
+      summarise(n = n (),
+                media = mean(altura, na.rm = TRUE),
+                dp = sd (altura, na.rm = TRUE),
+                mediana = median (altura, na.rm = TRUE),
+                IIQ = IQR (altura, na.rm = TRUE),
+                ic = plotrix::std.error(altura, na.rm = TRUE)*qt(1 - (0.05/2), n - 1),
+                linf = media - ic,
+                lsup = media + ic)
 }
+
