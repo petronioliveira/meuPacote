bf119785920e3f1cc69e8eee0e8c642b9f5a895d
Petronio Fagundes de Oliveira Filho <petronioliveira@gmail.com>
2022-09-11 09:39:48 -0300

    Commit atualização 11/09 9h39min

--- R/resumo.R
+++ R/resumo.R
@@ -1,6 +1,6 @@
 #' Função para calcular medidas resumidoras com gráficos
 #'
-#' Esta função calcula medias resumidoras (média, desvio padrão, mediana, P25 e
+#' Esta função calcula medidas resumidoras (média, desvio padrão, mediana, P25 e
 #' P75), entregando junto um histograma com curva normal sobreposta e boxplot de
 #' uma variável numérica
 #'
@@ -9,14 +9,22 @@
 #' @param x é um vetor de uma variável numérica
 #' @param lab é o rótulo da variável númerica, escrita entre aspas
 #'
+#'@examples
+#'library (readxl)
+#'library (dplyr)
+#'
+#'dados <- read_excel ("C:/Users/petro/Dropbox/Estatística/Bioestatística usando o R/dadosMater.xlsx") %>%
+#'         select (altura)
+#'
+#'resumo(dados$altura, lab = "Altura (m)")
 #'
 #'@export
 resumo <- function(x, lab) {
-  media = round(mean (x, na.rm = TRUE),2)
-  dp = round(sd (x, na.rm = TRUE), 2)
-  mediana = round(median (x, na.rm = TRUE), 2)
-  P25 = round(quantile (x, 0.25), 2)
-  P75 = round(quantile (x, 0.75), 2)
+  media = round(mean (x, na.rm = TRUE),3)
+  dp = round(sd (x, na.rm = TRUE), 3)
+  mediana = round(median (x, na.rm = TRUE), 3)
+  P25 = round(quantile (x, 0.25, na.rm = TRUE), 3)
+  P75 = round(quantile (x, 0.75, na.rm = TRUE), 3)
   tab <- matrix (c(media, dp, mediana, P25, P75))
   dimnames(tab) <- list(c("Média", "Desvio Padrão", "Mediana", "P25", "P75"),
                         "Valores")
@@ -26,7 +34,7 @@ resumo <- function(x, lab) {
           col="red",
           xlab= lab,
           ylab = "Frequência",
-          main="Histograma com Curva Normal")
+          main="")
   xfit<-seq(min(x),max(x),length=40)
   yfit<-dnorm(xfit,mean=mean(x),sd=sd(x))
   yfit <- yfit*diff(h$mids[1:2])*length(x)
